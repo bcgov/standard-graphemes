b05db8ab84a92cd944336d3703a090fdec7ab036
Tyler Krys <Tyler.Krys@gov.bc.ca>
2025-02-20 15:25:05 -0800

    Add links to confusables page and character explorer page in Header

--- validator/frontend/src/App.tsx
+++ validator/frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { PropsWithChildren } from "react";
 import { Footer, Header } from "@bcgov/design-system-react-components";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Link } from "@tanstack/react-router";
 
 import "./App.css";
 
@@ -9,7 +10,10 @@ const queryClient = new QueryClient();
 function App({ children }: PropsWithChildren) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Header title="Standard Graphemes" />
+      <Header title="Standard Graphemes">
+        <Link to="/">Confusables</Link>
+        <Link to="/character">Character Explorer</Link>
+      </Header>
       {children}
       <Footer />
     </QueryClientProvider>
