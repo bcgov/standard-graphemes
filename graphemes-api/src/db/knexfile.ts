d637698caa81796254d483aed0409e00a08e846e
Tyler Krys <Tyler.Krys@gov.bc.ca>
2025-05-12 15:37:14 -0700

    Add knexfile with development and production database connections

---
+++ graphemes-api/src/db/knexfile.ts
@@ -0,0 +1,35 @@
+import dotenv from "dotenv";
+dotenv.config();
+import type { Knex } from "knex";
+
+const DATABASE_URL = process.env.DATABASE_URL;
+
+if (!DATABASE_URL) {
+  throw new Error("DATABASE_URL environment variable is undefined.");
+}
+
+const config: { [key: string]: Knex.Config } = {
+  // Local development
+  development: {
+    client: "postgresql",
+    connection: {
+      connectionString: DATABASE_URL,
+      ssl: false,
+    },
+  },
+
+  // OpenShift deployment
+  production: {
+    client: "postgresql",
+    connection: {
+      connectionString: DATABASE_URL,
+      ssl: true,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  },
+};
+
+export default config;
