0b7147f8f2b66d03b509456340525dce7d7b7041
Tyler Krys <Tyler.Krys@gov.bc.ca>
2025-05-12 15:39:46 -0700

    Count rows in graphemes table in API route as test of database connection

--- graphemes-api/src/routes/api/index.ts
+++ graphemes-api/src/routes/api/index.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 
+import db from "../../db/index.js";
+
 const apiRouter = Router();
 
 // GET /api
-apiRouter.get("/", (req, res) => {
-  res.json({ message: "API" });
+apiRouter.get("/", async (req, res) => {
+  const rows = await db<{ count: string }>("grapheme").count();
+  const count = rows[0]?.count;
+  console.log("count: ", count);
+
+  res.json({ message: `API, count of graphemes: ${count}` });
 });
 
 export default apiRouter;
