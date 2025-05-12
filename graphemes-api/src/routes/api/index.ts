a4a80abe1415b0c43c5b0684ca2311c45e7b995c
Tyler Krys <Tyler.Krys@gov.bc.ca>
2025-05-12 15:57:58 -0700

    Add try/catch around graphemes count in /api route

--- graphemes-api/src/routes/api/index.ts
+++ graphemes-api/src/routes/api/index.ts
@@ -6,11 +6,16 @@ const apiRouter = Router();
 
 // GET /api
 apiRouter.get("/", async (req, res) => {
-  const rows = await db<{ count: string }>("grapheme").count();
-  const count = rows[0]?.count;
-  console.log("count: ", count);
+  try {
+    const rows = await db<{ count: string }>("grapheme").count();
+    const count = rows[0]?.count;
+    console.log("count: ", count);
 
-  res.json({ message: `API, count of graphemes: ${count}` });
+    res.json({ message: `API, count of graphemes: ${count}` });
+  } catch (error) {
+    console.error("Error fetching grapheme count:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 export default apiRouter;
