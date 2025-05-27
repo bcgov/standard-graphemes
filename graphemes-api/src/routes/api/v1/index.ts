fe1e5e063666605f142339fd963759bc2bb13753
Tyler Krys <Tyler.Krys@gov.bc.ca>
2025-05-26 17:34:10 -0700

    Add /api/v1/match POST route

--- graphemes-api/src/routes/api/v1/index.ts
+++ graphemes-api/src/routes/api/v1/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 
 import languagesRouter from "./languages/index.js";
+import matchRouter from "./match/index.js";
 
 const v1Router = Router();
 v1Router.use("/languages", languagesRouter);
+v1Router.use("/match", matchRouter);
 
 export default v1Router;
