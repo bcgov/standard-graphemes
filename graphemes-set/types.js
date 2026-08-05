183e9d0afe777092ba246cbae3a25606140ff70a
Tyler Krys <Tyler.Krys@gov.bc.ca>
2026-08-05 15:40:27 -0700

    Remove type generation comment

--- graphemes-set/types.js
+++ graphemes-set/types.js
@@ -14,25 +14,3 @@
  * @property {unknown | undefined} octokit
  * @property {GithubSourceConfigRepo} repo
  */
-
-// const githubToken = process.env.GITHUB_TOKEN?.trim();
-// const octokit = new Octokit(
-//   githubToken
-//     ? {
-//         auth: githubToken,
-//       }
-//     : undefined,
-// );
-// const REPO_OWNER = "First-Peoples-Cultural-Council";
-// const REPO_NAME = "unicode-resources";
-// const BASE_PATH = "orthography-resources";
-// const LANGUAGES_METADATA_FILE = "firstvoices_sites_metadata_2025.csv";
-// const githubSourceConfig = {
-//   githubToken,
-//   octokit,
-//   repo: {
-//     owner: REPO_OWNER,
-//     name: REPO_NAME,
-//     languagesMetadataFile: LANGUAGES_METADATA_FILE,
-//   },
-// };
