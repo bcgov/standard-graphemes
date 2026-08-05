889ed50e784286b240d5851520b79f1ca9f4d4ee
Tyler Krys <Tyler.Krys@gov.bc.ca>
2026-08-05 15:35:08 -0700

    Fix surrogate-pair logic in getUnicodeEscapes() - no change to output

--- graphemes-set/utils/getUnicodeEscapes.js
+++ graphemes-set/utils/getUnicodeEscapes.js
@@ -4,10 +4,12 @@
  * @returns {string} Ex: `\u0041\u0042\u0043`
  */
 export function getUnicodeEscapes(str) {
-  return [...str]
-    .map((char) => {
-      const code = char.codePointAt(0).toString(16).toUpperCase();
-      return `\\u${code.padStart(4, "0")}`;
-    })
+  return Array.from(str)
+    .map((char) =>
+      Array.from({ length: char.length }, (_, i) => {
+        const code = char.charCodeAt(i).toString(16).toUpperCase();
+        return `\\u${code.padStart(4, "0")}`;
+      }).join(""),
+    )
     .join("");
 }
